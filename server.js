8e0e6a017e47295e295c8d95178b73e9e11c6f4c
Rodihat <74569318+Rodihat@users.noreply.github.com>
2021-01-27 20:47:43 +0000

    Adding /get and /post routes

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use('/images', express.static(__dirname + '/static/coursework'), (req, res, 
 })
 
 
+
 //Logger middleware
 app.use((req, res, next) => {
     console.log("Request IP -", req.ip);
@@ -25,5 +26,39 @@ app.use((req, res, next) => {
     next();
 })
 app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Homepage');
+});
+
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
+//Retrieve collection from database
+//toArray() gives the actual result as find() only points to cursor of array
+app.get('/collection/:collectionName', (req, res, next) => {
+    let resultt;
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next (e)
+        res.send(results)
+        resultt = JSON.stringify(results);
+        console.log(resultt)
+    })
+});
+
+//To post to server
+app.post('/collection/:collectionName', (req, res, next) => {
+    
+    req.collection.insertOne(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+        let resultt = req.body
+        console.log("Order details")
+        console.log(JSON.stringify(resultt))
+    })
+});
+
 
 app.listen(3000);
